91ae771bc54f91ed20ba9a11b5a4291faaa5a754
unknown <anzarsh@mail.ru>
2017-02-13 16:59:43 +0300

    add 1

--- includes/vigoda.php
+++ includes/vigoda.php
@@ -2,7 +2,7 @@
 	<div class="container">
 		<div class="row">
 			<div class="col-md-12">
-				
+				Кому выгодна
 			</div>
 		</div>
 	</div>
--- includes/zakaz.php
+++ includes/zakaz.php
@@ -2,7 +2,7 @@
 	<div class="container">
 		<div class="row">
 			<div class="col-md-12">
-				
+				Почему заказывают 
 			</div>
 		</div>
 	</div>
