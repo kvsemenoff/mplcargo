7e0f6fd73a8e2bbe3405f2a984fe3af35a9e5459
Berzeg07 <berezg86@gmail.com>
2017-02-14 10:42:16 +0300

    29.14

--- includes/header-top.php
+++ includes/header-top.php
@@ -1,8 +1,20 @@
-<section class="section section__padding">
+<section class="section section__padding section__header">
 	<div class="container">
 		<div class="row">
-			<div class="col-md-12">
-				
+			<div class="dd-logo-wrap">
+				<div class="dd-logo-img">
+					
+				</div>	
+				<div class="dd-logo-txt">
+					<span>Международная доставка</span>
+				</div>
+			</div>
+			<div class="dd-menu-wrap">
+				<ul>
+					<li>География работы</li>
+					<li>О компании</li>
+					<li>Контакты</li>
+				</ul>
 			</div>
 		</div>
 	</div>
--- tech.php
+++ tech.php
@@ -1,13 +1,16 @@
 ﻿<?php require_once('includes/header.php'); ?>
 
 <br><br>		
-<a href="#" class="button-1">Узнать больше о гарантиях</a>	
+<a href="#" class="button-1 button-1__padding">Узнать больше о гарантиях</a>	
 <br><br>	
-<h2>Пример заголовка</h2>
-<br><br>	
-<form action="#">
-	<input type="text" value="" placeholder="Инпут тайп текст">		
-	<input type="submit" value="Значение" placeholder="Инпут тайп сабмит">		
+
+<h2>Пример заголовка h2</h2>
+
+<br><br>
+
+<form class="common-form" action="#">
+	<input class="input__text input__text__height" type="text" value="" placeholder="Инпут тайп текст">		
+	<input class="submit__view submit__view__padding" type="submit" value="Заказать звонок" placeholder="">		
 </form>		
 
 
