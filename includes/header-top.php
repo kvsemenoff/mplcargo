d708a9f2e47004f0d4151c5fcb8b98c04c19171d
Berzeg07 <berezg86@gmail.com>
2017-02-14 15:31:38 +0300

    29.14

--- includes/header-top.php
+++ includes/header-top.php
@@ -1,21 +1,72 @@
 <section class="section section__padding section__header">
-	<div class="container">
-		<div class="row">
-			<div class="dd-logo-wrap">
-				<div class="dd-logo-img">
-					
-				</div>	
-				<div class="dd-logo-txt">
-					<span>Международная доставка</span>
+	<div class="dd-header-top-wrap">
+		<div class="container">
+			<div class="row">
+
+				<div class="dd-logo-wrap dd-float__left margin-center">
+					<div class="dd-logo-img dd-float__left_min">
+						<img src="img/logo.png" alt="">
+					</div>	
+					<div class="dd-logo-txt dd-float__left_min">
+						<span>Международная<br> доставка</span>
+					</div>
+					<div class="clearfix"></div>
 				</div>
+				<div class="dd-menu-wrap dd-float__left text__center">
+					<ul>
+						<li><a href="#">География работы</a></li>
+						<li><a href="#">О компании</a></li>
+						<li><a href="#">Контакты</a></li>
+					</ul>
+				</div>
+
+				<!-- Правая часть -->
+				<div class="dd-right-wrap dd-float__left">
+					<div class="dd-green text__center dd-float__left ">
+						<span>Звонок по России, турции<br>
+							и Италии бесплатный
+						</span>
+					</div>
+					<div class="dd-numbers dd-float__left">
+						<div class="dd-numbers__box number__style text__center dd-float__left">
+							<span class="rus-number">8 800 350 02 05</span>
+						</div>
+						<div class="dd-numbers__box text__center dd-float__left">
+							<div class="dd-tyrkey text__center">
+								<a class="number__style turkey-number" href="#">+90 212 900 89 56</a>
+							</div>
+							<!-- <div class="dd-italy text__center">
+								<a class="number__style" href="#">+88 888 988 89 56</a>
+							</div> -->
+							<div class="dd-links-h-w">	
+								<a class="dd-links-h" href="#">Турция</a>
+								<a class="dd-links-h" href="#">Италия</a>
+							</div>
+						</div>	
+						
+						<div class="clearfix"></div>	
+					</div>
+				</div>
+				<div class="dd-numbers__box text__center dd-float__right">
+					<div class="dd-soc-ico ">
+						<span><img src="img/whatsapp_logo.png" alt=""></span>
+						<span><img src="img/viber_logo.png" alt=""></span>
+						<span><img src="img/skype_logo.png" alt=""></span>
+					</div>
+					<span class="number__style">+7 921 8914384</span><br>
+					<a class="dd-links-h" href="#">Заказать звонок</a>
+				</div>	
+				<div class="clearfix"></div>
+				<!-- конец правой части -->
 			</div>
-			<div class="dd-menu-wrap">
-				<ul>
-					<li>География работы</li>
-					<li>О компании</li>
-					<li>Контакты</li>
-				</ul>
-			</div>
+		</div>
+	</div>
+	<div class="dd-main-header-wrap">
+		<div class="dd-main-txt">
+			<span>Доставка одежды
+				из Турции от 50 кг
+				за 6 дней 
+			</span>
 		</div>
 	</div>
 </section>
