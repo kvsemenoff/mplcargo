96bd068477ea99ea8957ccbcd6aa4e91342b731a
engine77 <zaur.ka.00@mail.ru>
2017-02-14 10:52:43 +0300

    14

--- includes/delivery.php
+++ includes/delivery.php
@@ -1,9 +1,26 @@
-<section class="section section__padding">
-	<div class="container">
-		<div class="row">
-			<div class="col-md-12">
-				Доставка
-			</div>
+<section class="section section__padding df-border">
+	<div class="container df-container">
+		<div class="df-delivery df-delivery_view">
+			<h3 class="df-deliverycap">Доставка из Турции за 3 шага <br> и Вы уже продаете товар</h3>
+				<div class="df-deliverybox df-deliverybox_view">
+					<span class="dfnumber dfnumber_view">1</span>
+					<div class="df-deliveryimg df-deliveryimg_view"><img src="img/delivery_stambul.jpg" alt=""></div>
+					<span class="df-deliverytxt1 df-deliverytxt1_view">Отправка из Стамбула</span>
+					<span class="df-deliverytxt2 df-deliverytxt2_view">С нашего склада из г. Стамбул 2 раза в неделю мы отправляем грузы в Москву.</span>
+				</div>
+				<div class="df-deliverybox df-deliverybox_view">
+					<span class="dfnumber dfnumber_view">2</span>
+					<div class="df-deliveryimg df-deliveryimg_view"><img src="img/delivery_moscow.jpg" alt=""></div>
+					<span class="df-deliverytxt1 df-deliverytxt1_view">6 дней и Ваш товар в Москве</span>
+					<span class="df-deliverytxt2 df-deliverytxt2_view">Пока товар в пути мы выясняем, куда нужно осуществить доставку из Москвы. Ваш персональный менеджер всегда на связи, в любое время готов дать информацию о статусе груза и ответить на вопросы</span>
+				</div>
+				<div class="df-deliverybox df-deliverybox_view">
+					<span class="dfnumber dfnumber_view">3</span>
+					<div class="df-deliveryimg df-deliveryimg_view"><img src="img/delivery_key.jpg" alt=""></div>
+					<span class="df-deliverytxt1 df-deliverytxt1_view">Доставка «Под Ключ»</span>
+					<span class="df-deliverytxt2 df-deliverytxt2_view">После прибытия в Москву ваш товар будет отправлен удобным для вас способом: автотранспортом, самолетом или автобусом. Подскажем наиболее дешевый и быстрый способ доставки одежды до вашего адреса учитывая ваши предпочтения.</span>
+				</div>
+			
 		</div>
 	</div>
 </section>
