996d594d7ecc4eb7a1fbb3e3ffa49d8893de92c1
unknown <anzarsh@mail.ru>
2017-02-14 11:43:14 +0300

    add 1

--- includes/garant.php
+++ includes/garant.php
@@ -7,27 +7,27 @@
 			<div class="col-md-4">
 				<img src="img/standard.png" alt="img">
 				<p class="db-garant-p">
-					<span>Стандартная страховка</span>
+					<span>Стандартная страховка</span> <br>
 					Если ваш груз не застрахован и до начала перевозки не <br> предоставлен инвойс груз страхуется согласно стандартам<br> CMR конвенции.
 				</p>
 			</div>	
 			<div class="col-md-4">
 				<img src="img/extra.png" alt="img">
 				<p class="db-garant-p">
-					<span>Дополнительная страховка</span>
+					<span>Дополнительная страховка</span> <br>
 					Стоимость 2% от цены товаров, компенсация 100% при<br> наступлении страхового случая.
 				</p>
 			</div>
 			<div class="col-md-4">
 				<img src="img/standard.png" alt="img">
 				<p class="db-garant-p">
-					<span>На рынке с 2008 года</span>
+					<span>На рынке с 2008 года</span><br>
 					9 лет успешной работы. На своем пути преодолели много <br> кризисов. Понимаем и учитываем все нюансы при<br> международной доставке.
 				</p>
 			</div>
 			<div class="clearfix"></div>
 			<div class="col-md-12">
-				<button class="button-1"> Заказать доставку со страховкой</button>
+				<button class="button-1 db-garant-button"> Заказать доставку со страховкой</button>
 			</div>
 		</div>
 	</div>
