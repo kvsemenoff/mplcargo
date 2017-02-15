21d4d307a8b101b39b538624ab3c24be19a3487f
Barasbi <betokov93@gmail.com>
2017-02-15 11:56:23 +0300

    carousel fix

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 $(document).ready(function(){
     $('.owl-carousel').owlCarousel({
         loop: true,
+        navText:['<span class="dg-left"></span>','<span class="dg-right"></span>'],
         responsive: {
             0: {
                 items: 3
