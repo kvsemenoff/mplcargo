056b736df91d8fb12c0dbf339e897005bd8af6e5
Berzeg07 <berezg86@gmail.com>
2017-02-15 17:40:00 +0300

    29.14

--- includes/header-top.php
+++ includes/header-top.php
@@ -5,7 +5,7 @@
 
 				<div class="dd-logo-wrap dd-float__left margin-center">
 					<div class="dd-logo-img dd-float__left_min">
-						<img src="img/logo.png" alt="">
+						<img src="img/mpl.png" alt="">
 					</div>	
 					<div class="dd-logo-txt dd-float__left_min">
 						<span>Международная<br> доставка</span>
@@ -29,18 +29,18 @@
 					</div>
 					<div class="dd-numbers dd-float__left">
 						<div class="dd-numbers__box number__style text__center dd-float__left">
-							<span class="rus-number">8 800 350 02 05</span>
+							<a class="rus-number" href="tel:+88003500205">8 800 350 02 05</a>
 						</div>
 						<div class="dd-numbers__box text__center dd-float__left">
-							<div class="dd-tyrkey text__center">
+							<div class="dd-tyrkey text__center tab__box2" id="tab3">
 								<a class="number__style turkey-number tr" href="#">+90 212 900 89 56</a>
 							</div>
-							<div class="dd-italy text__center">
+							<div class="dd-italy text__center tab__box2" id="tab4">
 								<a class="number__style it-number tr" href="#">+88 888 988 89 56</a>
 							</div>
-							<div class="dd-links-h-w">	
-								<a class="dd-links-h dd-links-h1 active" href="#">Турция</a>
-								<a class="dd-links-h dd-links-h2" href="#">Италия</a>
+							<div class="dd-links-h-w tab2">	
+								<a class="dd-links-h dd-links-h1 active" href="#tab3">Турция</a>
+								<a class="dd-links-h dd-links-h2" href="#tab4">Италия</a>
 							</div>
 						</div>	
 						
@@ -49,11 +49,11 @@
 				</div>
 				<div class="dd-numbers__box text__center dd-float__right">
 					<div class="dd-soc-ico ">
-						<span><img src="img/whatsapp_logo.png" alt=""></span>
-						<span><img src="img/viber_logo.png" alt=""></span>
-						<span><img src="img/skype_logo.png" alt=""></span>
+						<a href="tel:+89218914384"><img src="img/w.png" alt=""></a>
+						<a href="tel:+89218914384"><img src="img/v.png" alt=""></a>
+						<a href="tel:+89218914384"><img src="img/s.png" alt=""></a>
 					</div>
-					<span class="number__style">+7 921 8914384</span><br>
+					<span class="number__style">+7 921 891 43 84</span><br>
 					<a class="dd-links-h" href="#log-in1" name="modal">Заказать звонок</a>
 				</div>	
 				<div class="clearfix"></div>
@@ -66,24 +66,31 @@
 		<div class="row">
 
 			<div class="dd-main-header-wrap">
+				<div class="dd-bl">
+					<img src="img/bl.png" alt="">
+				</div>
 				<div class="dd-main-txt">
-					<span>Доставка одежды
-						из Турции от 50 кг
-						за 6 дней 
+					<span>Оборот бизнеса +30%<br>
+						Благодоря доставке одежды<br>
+						Из Турции за 6 дней
 					</span>
 				</div>
 				<div class="dd-slogan">
-					<span>по лучшей цене в России</span>
+					<span>По лучшей цене в России. Грузы от 30 кг</span>
+				</div>
+				<div class="dd-wh">
+					<span>Отправка строго по расписанию</span><br>
+					<span>2 раза в неделю из Стамбула</span>
 				</div>
 				<div class="dd-paragraf">
-					<p>— Надоело что из-за задержек транспортных компаний ваш бизнес теряет прибыль ?</p>
-					<p>— повысьте товарооборот своего магазина на 30% благодаря быстрому и предсказуемому поступлению Нового товара Из Турции</p>
+					<p>Надоело что из-за задержек транспортных<br> компаний ваш бизнес теряет прибыль ?</p>
 				</div>
+
 				<div class="dd-button-header-wrap">
 					<a class="button-1 button-1__padding" name="modal" href="#log-in1">Заказать бесплатную консультацию</a>
 				</div>
 			</div><!-- dd-main-header-wrap -->
-			
+
 		</div><!-- Row -->
 	</div><!-- Container -->
 </section>
