8d8678cf7c703e0dff01e04eb62ad3225752a31d
engine77 <zaur.ka.00@mail.ru>
2017-02-15 15:18:14 +0300

    15

--- js/common.js
+++ js/common.js
@@ -73,24 +73,27 @@ $(document).ready(function(){
     var maskWidth = $(window).width();
     $('#mask').css({'width':maskWidth,'height':maskHeight});
     $('#mask').fadeTo("slow",0.8); 
-    var winH = $(window).height();
+    var winH = $(window).height();   
     var winW = $(window).width();
     posTop = (window.pageYOffset !== undefined) ? window.pageYOffset : (document.documentElement ||document.body.parentNode || document.body).scrollTop;
-    $(id).css('top',  posTop+50);
+    $(id).css('top',  posTop);
     $(id).css('left', winW/2-$(id).width()/2);
-    $(id).fadeIn(500); 
+    $(id).css('height', winH);
+    $('html,body').css("overflow-y","hidden");
+    $(id).slideDown(500); 
 });
    $('.window .dd-close').click(function (e) {
     e.preventDefault();
     $('#mask, .window').hide();
     $('.window').hide();
+    $('html,body').removeAttr('style');
 }); 
 
    $('#mask, .an-exit__krest').click(function () {
     $('#mask').hide();
     $('.window').hide();
 }); 
-    // $(".phone").mask("+ 7 (999) 999 - 99 - 99?"); 
+    $(".phone").mask("+ 7 (999) 999 - 99 - 99?"); 
     $(".form1").submit(function() { 
         var tel = $(this).find('input[name="phone"]');
         var empty = false;
